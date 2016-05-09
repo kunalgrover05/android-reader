6b3cd704a104763407677f7b821d65631f786164
kunalgrover05 <kunalgrover05@gmail.com>
2016-05-09 09:25:56 +0530

    Using PDFView

--- app/src/main/java/com/kunal/reader/PDFViewFragment.java
+++ app/src/main/java/com/kunal/reader/PDFViewFragment.java
@@ -2,6 +2,7 @@ package com.kunal.reader;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
@@ -10,6 +11,8 @@ import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.joanzapata.pdfview.PDFView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -27,36 +30,20 @@ public class PDFViewFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        // Render for showing on the screen
+        PDFView pdfView = (PDFView) getActivity().findViewById(R.id.pdfview);
+
         // File to load
         File file = ((HomeActivity) getActivity()).current_file;
 
-        ParcelFileDescriptor pfd = null;
-        try {
-            pfd = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        PdfRenderer renderer = null;
-        try {
-            renderer = new PdfRenderer(pfd);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        PdfRenderer.Page page = renderer.openPage(0);
-
-        // Render for showing on the screen
-        ImageViewTouch pdfView = (ImageViewTouch) getActivity().findViewById(R.id.pdfpage);
-
-        DisplayMetrics displayMetrics = getActivity().getResources().getDisplayMetrics();
-        int screenWidth = displayMetrics.widthPixels;
-        int screenHeight = displayMetrics.heightPixels;
-
-        Bitmap mBitmap = Bitmap.createBitmap(screenWidth, screenHeight,
-                Bitmap.Config.ARGB_8888);
-        page.render(mBitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
-        pdfView.setImageBitmap(mBitmap);
-
-        page.close();
-        renderer.close();
+        pdfView.fromFile(file)
+                .defaultPage(0)
+                .showMinimap(true)
+                .enableSwipe(true)
+                .swipeVertical(true)
+//                .onDraw(onDrawListener)
+//                .onLoad(onLoadCompleteListener)
+//                .onPageChange(onPageChangeListener)
+                .load();
     }
 }
