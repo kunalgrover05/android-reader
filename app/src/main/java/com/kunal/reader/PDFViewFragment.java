c8ab5e65829d997ca3fd9fe5d50ed5ca72a04990
kunalgrover05 <kunalgrover05@gmail.com>
2016-05-21 22:45:57 +0530

    Added native PDF view

--- app/src/main/java/com/kunal/reader/PDFViewFragment.java
+++ app/src/main/java/com/kunal/reader/PDFViewFragment.java
@@ -6,8 +6,12 @@ import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
 import android.util.DisplayMetrics;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,10 +22,16 @@ import java.io.IOException;
 import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
 public class PDFViewFragment extends Fragment {
+    private int currentPage;
+    private PdfRenderer renderer;
+
+    private GestureDetectorCompat mDetector;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pdf_viewer, container, false);
+        View view = inflater.inflate(R.layout.pdf_viewer, container, false);
+        return view;
     }
 
     @Override
@@ -36,27 +46,41 @@ public class PDFViewFragment extends Fragment {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        PdfRenderer renderer = null;
+
         try {
             renderer = new PdfRenderer(pfd);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        PdfRenderer.Page page = renderer.openPage(0);
+
+        currentPage = 0;
+        loadPage();
+    }
+
+    private void loadPage() {
+        PdfRenderer.Page page = renderer.openPage(currentPage);
 
         // Render for showing on the screen
         ImageViewTouch pdfView = (ImageViewTouch) getActivity().findViewById(R.id.pdfpage);
+        pdfView.setSingleTapListener(
+            new ImageViewTouch.OnImageViewTouchSingleTapListener() {
+                @Override
+                public void onSingleTapConfirmed() {
+                    currentPage += 1;
+                    loadPage();
+                }
+            }
+        );
 
         DisplayMetrics displayMetrics = getActivity().getResources().getDisplayMetrics();
         int screenWidth = displayMetrics.widthPixels;
         int screenHeight = displayMetrics.heightPixels;
 
-        Bitmap mBitmap = Bitmap.createBitmap(screenWidth, screenHeight,
+        Bitmap mBitmap = Bitmap.createBitmap(screenWidth * 2, screenHeight * 2,
                 Bitmap.Config.ARGB_8888);
         page.render(mBitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
         pdfView.setImageBitmap(mBitmap);
 
         page.close();
-        renderer.close();
     }
 }
